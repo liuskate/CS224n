be79eea4fe3e24b457c31bf7815ac6e99a7e7bcb
liuskate <liuskate@gmail.com>
2018-01-10 15:02:52 +0800

    add q2_neural.py

---
+++ q2_neural.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+
+import numpy as np
+import random
+
+from q1_softmax import softmax
+from q2_sigmoid import sigmoid, sigmoid_grad
+from q2_gradcheck import gradcheck_naive
+
+
+def forward_backward_prop(data, labels, params, dimensions):
+    """
+    Forward and backward propagation for a two-layer sigmoidal network
+
+    Compute the forward propagation and for the cross entropy cost,
+    and backward propagation for the gradients for all parameters.
+
+    Arguments:
+    data -- M x Dx matrix, where each row is a training example.
+    labels -- M x Dy matrix, where each row is a one-hot vector.
+    params -- Model parameters, these are unpacked for you.
+    dimensions -- A tuple of input dimension, number of hidden units
+                  and output dimension
+    """
+
+    ### Unpack network parameters (do not modify)
+    ofs = 0
+    Dx, H, Dy = (dimensions[0], dimensions[1], dimensions[2])
+
+    W1 = np.reshape(params[ofs:ofs+ Dx * H], (Dx, H))
+    ofs += Dx * H
+    b1 = np.reshape(params[ofs:ofs + H], (1, H))
+    ofs += H
+    W2 = np.reshape(params[ofs:ofs + H * Dy], (H, Dy))
+    ofs += H * Dy
+    b2 = np.reshape(params[ofs:ofs + Dy], (1, Dy))
+
+    ### YOUR CODE HERE: forward propagation
+    #raise NotImplementedError
+    z1 = np.dot(data, W1) + b1              # M * H
+    h = sigmoid(z1)                         # M * H
+    z2 = np.dot(h, W2) + b2                 # M * Dy
+    yhat = softmax(z2)                      # M * Dy
+    ### END YOUR CODE
+
+    ### YOUR CODE HERE: backward prop
+    # cost : CE(y, yhat) = -sum(y * log(yhat))
+
+    # average of cost
+    cost = np.sum(-np.log(yhat[labels==1])) / labels.shape[0]   # scalar
+    gradz2 = (yhat - labels) / labels.shape[0]                     # M*Dy
+    gradW2 = np.dot(h.T, gradz2)                                # h: M*H  W2: H*Dy
+    gradb2 = np.sum(gradz2, 0)                                                 # 1*Dy
+
+    gradh = np.dot(gradz2, W2.T)                            # W2: H*Dy  h: M*H
+    gradz1 = gradh * sigmoid_grad(h)                       # M*H
+    gradW1 = np.dot(data.T, gradz1)                                       # data M*Dx  W1:Dx*H
+    gradb1 = np.sum(gradz1, 0)
+    ### END YOUR CODE
+
+    ### Stack gradients (do not modify)
+    grad = np.concatenate((gradW1.flatten(), gradb1.flatten(),
+            gradW2.flatten(), gradb2.flatten()))
+    return cost, grad
+
+
+def sanity_check():
+    """
+    Set up fake data and parameters for the neural network, and test using
+    gradcheck.
+    """
+    print "Running sanity check..."
+
+    N = 20
+    dimensions = [10, 5, 10]
+    data = np.random.randn(N, dimensions[0])   # each row will be a datum
+    labels = np.zeros((N, dimensions[2]))
+    for i in xrange(N):
+        labels[i, random.randint(0,dimensions[2]-1)] = 1
+
+    params = np.random.randn((dimensions[0] + 1) * dimensions[1] + (
+        dimensions[1] + 1) * dimensions[2], )
+
+    gradcheck_naive(lambda params:
+        forward_backward_prop(data, labels, params, dimensions), params)
+
+
+def your_sanity_checks():
+    """
+    Use this space add any additional sanity checks by running:
+        python q2_neural.py
+    This function will not be called by the autograder, nor will
+    your additional tests be graded.
+    """
+    print "Running your sanity checks..."
+    ### YOUR CODE HERE
+    raise NotImplementedError
+    ### END YOUR CODE
+
+
+if __name__ == "__main__":
+    sanity_check()
+    #your_sanity_checks()
